9fc94f8946c381bb400d1f446af68e897550c8f3
Trinitty999 <hanansmani@gmail.com>
2023-12-02 16:29:51 +0100

    still testing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,73 @@
-use ratatui::prelude::*;
+use crossterm::*;
+use crossterm::style::*;
+use crossterm::terminal::*;
+use crossterm::cursor::*;
+use crossterm::event::*;
+use libparted::*;
 
-fn main(){
-  let mut app = App::new();
-  
+use std::io::*;
+use std::process;
+
+fn list() -> Result<()> {
+    for (dev_i, mut device) in Device::devices(true).enumerate() {
+        let hw_geom = device.hw_geom();
+        let bios_geom = device.bios_geom();
+        
+        style::SetForegroundColor(style::Color::Cyan);
+
+        println!("Device {}", dev_i);
+        println!("    Model:         {:?}", device.model());
+        println!("    Path:          {:?}", device.path());
+        println!("    Sectors:       {}", device.length());
+        println!("    Sector Size:   {}", device.sector_size());
+        println!("    Type:          {:?}", device.type_());
+        println!("    Open Count:    {}", device.open_count());
+        println!("    Read Only:     {}", device.read_only());
+        println!("    External Mode: {}", device.external_mode());
+        println!("    Dirty:         {}", device.dirty());
+        println!("    Boot Dirty:    {}", device.boot_dirty());
+        println!("    Hardware Geometry:");
+        println!("        Cylinders: {}", hw_geom.cylinders);
+        println!("        Heads:     {}", hw_geom.heads);
+        println!("        Sectors:   {}", hw_geom.sectors);
+        println!("    BIOS Geometry:");
+        println!("        Cylinders: {}", bios_geom.cylinders);
+        println!("        Heads:     {}", bios_geom.heads);
+        println!("        Sectors:   {}", bios_geom.sectors);
+        println!("    Host:          {}", device.host());
+        println!("    Did:           {}", device.did());
+
+        let disk = Disk::new(&mut device)?;
+        eprintln!("    Disk Type:    {:?}", disk.get_disk_type_name());
+
+        for part in disk.parts() {
+            println!("    Part {}", part.num());
+            println!("        Type Name: {:?}", part.type_get_name());
+            println!("        Name:      {:?}", part.name());
+            println!("        Path:      {:?}", part.get_path());
+            println!("        Active:    {}", part.is_active());
+            println!("        Busy:      {}", part.is_busy());
+            println!("        FS:        {:?}", part.fs_type_name());
+            println!("        Start:     {}", part.geom_start());
+            println!("        End:       {}", part.geom_end());
+            println!("        Length:    {}", part.geom_length());
+        }
+    }
+    
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    execute!(
+        stdout(),
+        style::SetAttribute(style::Attribute::Bold),
+        terminal::Clear(terminal::ClearType::All),
+        SetForegroundColor(Color::Cyan),
+        terminal::SetTitle("Petya Unlocker"),
+        Print("Test.\n")
+    )?;
+    list();
+    
+    Ok(())
 }
